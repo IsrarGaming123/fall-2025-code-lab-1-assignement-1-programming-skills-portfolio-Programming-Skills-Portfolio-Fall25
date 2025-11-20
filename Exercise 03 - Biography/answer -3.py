c38566be45b7288cc0d0e1e6fade78d5f0724477
IsrarGaming123 <77373602+IsrarGaming123@users.noreply.github.com>
2025-11-20 09:43:19 +0400

    Add files via upload

---
+++ Exercise 03 - Biography/answer -3.py
@@ -0,0 +1,6 @@
+First_name = input("Enter your first name: ") #asking user for first name
+Last_name = input("Enter your last name: ")#asking user for last name
+name = First_name + " " + Last_name   #combining first and last name
+Age = (input("Enter your age: "))     #asking user for age
+Home_town = input("Enter your home town: ")          #asking user for home town
+print("my name is " + name + ",\nI am " + Age + " years old \nI come from " + Home_town + ".") #printing the values
