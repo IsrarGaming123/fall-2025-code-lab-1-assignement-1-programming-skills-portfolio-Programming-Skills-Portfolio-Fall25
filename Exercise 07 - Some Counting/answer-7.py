d9a159295c209a25891908dc35ff41c19392b433
IsrarGaming123 <77373602+IsrarGaming123@users.noreply.github.com>
2025-11-20 09:47:48 +0400

    Add files via upload

---
+++ Exercise 07 - Some Counting/answer-7.py
@@ -0,0 +1,37 @@
+def count1():
+    # Counts up from 0 to 50 in increments of 1
+    print("Output of count1 (0 to 50):")
+    for i in range(0, 51, 1):
+        print(i)
+
+def count2():
+    # Counts down from 50 to 0 in decrements of 1
+    print("Output of count2 (50 to 0):")
+    for i in range(50, -1, -1):
+        print(i)
+
+def count3():
+    # Counts up from 30 to 50 in increments of 1
+    print("Output of count3 (30 to 50):")
+    for i in range(30, 51, 1):
+        print(i)
+
+def count4():
+    # Counts down from 50 to 10 in decrements of 2
+    print("Output of count4 (50 to 10 by 2):")
+    for i in range(50, 9, -2):
+        print(i)
+
+def count5():
+    # Counts up from 100 to 200 in increments of 5
+    print("Output of count5 (100 to 200 by 5):")
+    for i in range(100, 201, 5):
+        print(i)
+
+
+#call functions form here
+#count1()
+#count2()
+#count3()
+#count4()
+#count5()
